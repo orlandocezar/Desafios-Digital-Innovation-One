6b208dd61b8413df3233448dd8efa1ddd63b6426
krobaltocezarx <orlando.cezarx@gmail.com>
2021-09-13 01:47:38 -0300

    Validação da nota

---
+++ Desafios Númericos/Validação da nota.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bootcamp {
+    class Program {
+        static void Main(string[] args) {
+
+
+            double somaDasNotas = 0;
+            int contador = 0;
+
+            do {
+                double notaEntradaConsole = double.Parse(Console.ReadLine());
+
+                if (notaEntradaConsole < 0 || notaEntradaConsole > 10) {
+                    Console.WriteLine("nota invalida");
+                } else {
+                    contador++;
+                    somaDasNotas += notaEntradaConsole;
+                }
+            } while (contador < 2);
+
+            Console.WriteLine("media = " + (somaDasNotas / 2).ToString("N2"));
+        }
+    }
+}
+
