e454889f4483702fa8fedf6c080a92497bbd7806
krobaltocezarx <orlando.cezarx@gmail.com>
2021-09-13 01:28:08 -0300

    Maior

---
+++ Desafios Númericos/Maior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bootcamp {
+    class Program {
+        static void Main(string[] args) {
+
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+            int c = int.Parse(vet[2]);
+            //complete declarando as variáveis
+
+
+            int MAIORAB = (a + b + Math.Abs(a - b)) / 2;
+
+            if (MAIORAB > c) {
+                Console.WriteLine($"{MAIORAB} eh o maior");
+            } else {
+                Console.WriteLine($"{c} eh o maior");
+            }
+
+
+
+
+
+        }
+    }
+}
